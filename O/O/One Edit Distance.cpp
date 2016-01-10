e5d4bb889c4d9332b61daf09748eabb859eded13
QINQIN ZHU <qinqinzhu@dhcp-169-233-164-206.cruznet.ucsc.edu>
2016-01-09 20:46:19 -0800

    string

--- O/O/One Edit Distance.cpp
+++ O/O/One Edit Distance.cpp
@@ -19,6 +19,20 @@
 #include<strstream>
 #include<strings.h>
 #include<sstream>
+/*
+thress cases:
+ //make sure m<n
+ 1.case 1:m==n,only need to modify the different one and ++i.then check if rest of the s[i] and t[i] are the same.
+     after first loop,if i==m and m==n return false(same);or i==m but m!=n return true;(insert the last one.)
+ case 2:n-m==1,only need to insert into s.then check if s[i] and t[i+1] are the same.
+ 
+ conclude 1 and 2:s[i]==t[i+shift]
+ 
+ 
+ 
+ 
+ 
+*/
 using namespace std;
 class Solution {
 public:
@@ -31,8 +45,11 @@ public:
         int c=n-m;
         int i=0;
         while(s[i]==t[i]&&i<m) ++i;
+        // if two string are the same (shift==0), return false
         if(i==m) return c>0;
+        // if n==m skip current char in s (modify operation in s)
         if(c==0) ++i;
+        // use shift to skip one char in t
         while(s[i]==t[i+c]&&i<m) ++i;
         return (i==m);
     }
